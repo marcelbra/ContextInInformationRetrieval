5ddebfb9a2d58cfe58e42e51744e71b412381fd1
marcelbra <marcelbraasch@gmail.com>
2020-09-03 01:17:25 +0200

    Performed experiments on context embedding

--- bertUtils.py
+++ bertUtils.py
@@ -1,12 +1,12 @@
 import torch
 from newspaper import Article
 from sentence_transformers import SentenceTransformer, util
-import numpy as np
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from typing import Dict, List, Tuple
 from torch import Tensor
 from context_concepts import contexts
+import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
 def get_corpus_from_links(urls: List[str]) -> Tuple[List[str], Tensor]:
@@ -28,6 +28,7 @@ def filter(corpus_embeddings: Tensor,
            top_k: int=5
            ) -> List[int]:
     cos_scores = util.pytorch_cos_sim(query_embedding, corpus_embeddings)[0]
+    cos_scores = cos_scores.cpu()
     top_results = np.argpartition(-cos_scores, range(top_k))[0:top_k]
     return top_results.tolist()
 
@@ -47,10 +48,56 @@ def post_filter(corpus_embeddings: Tensor,
             new_embeddings = torch.cat((new_embeddings, corpus_embeddings[int(item_index)].reshape(1, 768)), dim=0)
 
     new_scores = util.pytorch_cos_sim(topic_embedding, new_embeddings)[0]
+    new_scores = new_scores.cpu()
     top_results = np.argpartition(-new_scores, range(top_k))[0:top_k]
     order = new_order(curr_top_indices, top_results)
     return order
 
+def similarity_matrix(encoded_topics: List[Tensor]
+                      ) -> List[List[float]]:
+    topic_length = len(encoded_topics)
+    m = [[0] * topic_length for x in range(topic_length)]
+    for i, row in enumerate(encoded_topics):
+        for j, col in enumerate(encoded_topics):
+            m[i][j] = round(float(dist(row, col)), 4)
+    return m
+
+def dist(x, y):
+    return util.pytorch_cos_sim(x,y)[0]
+    # dist = lambda x, y: util.pytorch_cos_sim(x,y)[0]
+
+def dist_w(x, y):
+    return util.pytorch_cos_sim(en(x),en(y))[0]
+    # dist_w = lambda x, y: util.pytorch_cos_sim(en(x),en(y))[0]
+def en(x):
+    return model.encode(x)
+    # en = lambda x: model.encode(x)
+
+model = SentenceTransformer('distilbert-base-nli-stsb-mean-tokens')#distilbert-base-nli-mean-tokens')
+"""
+Experiment 001
+
+Setting:
+Taking a toy corpus of 10 corona articles. All articles talk about
+masks and some talk about masks w.r.t. kids and their environment (school).
+We assume a mother (role) that wants to know if her child should wear a mask (e.g. in
+school (location)). The location will be context modelled here (i.e. encoded through a wiki article)
+and and used in a post-processing manner.
+
+Hypothesis:
+The hypothesis is that having this knowledge about the mother and the setting
+she is interested in results in better information.
+
+Result:
+We can see that when we query we retrieve as results the ranking [5,4,0,9,8]. This shows that
+the general face mask articles are ranked 1 and 2. The third article is a kids article, however
+the two best articles which explicitly talk about what the mother wants to know are ranked low.
+After querying against that ranking with the school we retrieve a new ranking [0, 8, 9, 5, 4]
+which, without a doubt, would perfectly meet the mother's concern.
+
+The PCA visualizes that the documents indeed are very close to each other. It shows that in this
+semantic space only minor differences are responsible for worse or better results.
+"""
 # link collection
 urls = ["https://www.nytimes.com/2020/08/06/us/crayola-masks-kids.html",
 		"https://www.nytimes.com/aponline/2020/08/25/business/ap-eu-virus-outbreak-britain.html",
@@ -62,25 +109,19 @@ urls = ["https://www.nytimes.com/2020/08/06/us/crayola-masks-kids.html",
 		"https://www.nytimes.com/2020/08/14/parenting/schools-reopening-south-korea.html",
 		"https://www.nytimes.com/2020/06/23/well/family/children-masks-coronavirus.html",
 		"https://www.nytimes.com/2020/04/09/parenting/coronavirus-kids-masks.html"]
-
-model = SentenceTransformer('distilbert-base-nli-mean-tokens')
-
 # filter corpus with query and retrieve top k indices
 query_embedding = model.encode("should my child wear a face mask", convert_to_tensor=True)
 corpus, corpus_embeddings = get_corpus_from_links(urls)
 top_indices = filter(corpus_embeddings, query_embedding, 5)
-
 # query corpus with the topic
-school_text = "A primary school, junior school, elementary school or grade school is a school for children from about four to eleven years old, in which they receive primary or elementary education. It can refer to both the physical structure (buildings) and the organisation. Typically it comes after preschool, and before secondary school."
+school_text = "child"#A primary school, junior school, elementary school or grade school is a school for children from about four to eleven years old, in which they receive primary or elementary education. It can refer to both the physical structure (buildings) and the organisation. Typically it comes after preschool, and before secondary school."
 school_embedding = model.encode(school_text, convert_to_tensor=True)
 top_indices = post_filter(corpus_embeddings, school_embedding, top_indices)
-
-
-# visualize
+# visualisation
 pca = PCA(n_components=3)
 embeddings = corpus_embeddings + query_embedding + school_embedding
 emb = torch.cat((corpus_embeddings, query_embedding.reshape(1, 768), school_embedding.reshape(1, 768)), dim=0)
-pc = pca.fit_transform(emb)
+pc = pca.fit_transform(emb.cpu())
 x, y, z = pc[:,0], pc[:,1], pc[:,2]
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -91,11 +132,113 @@ for i in range(12):
         ax.scatter(x[i], y[i], z[i], c='b', marker='o')
     else:
         ax.scatter(x[i], y[i], z[i], c='r', marker='o')
-plt.show()
+# plt.show()
 
-title = "Social distancing"
-text = "Social distancing (also known as physical distancing) includes infection control actions intended to slow the spread of disease by minimising close contact between individuals. Methods include quarantines; travel restrictions; and the closing of schools, workplaces, stadiums, theatres, or shopping centres. Individuals may apply social distancing methods by staying at home, limiting travel, avoiding crowded areas, using no-contact greetings, and physically distancing themselves from others. Many governments are now mandating or recommending social distancing in regions affected by the outbreak. Non-cooperation with distancing measures in some areas has contributed to the further spread of the pandemic"
-emb1 = model.encode(title)
-emb2 = model.encode(text)
-cos_scores = util.pytorch_cos_sim(emb1, emb2)[0]
-s = 0
+"""
+Experiment 002
+
+Setting:
+We now look at the context "theme" and one of its topics "mask".
+As we are in latent high dimensional space we cannot make sense of representation of encoded words.
+Moreover, making sense of encoded sentences or documents cannot be made sense of even more.
+It is crucial to verify if encoded information are precise and if words, descriptions and texts that
+are supposedly close to each are so indeed.
+
+Hypothesis:
+The hypothesis is that encoded wikipedia articles comprise important knowledge and are a better representation
+of a certain concept than just a word alone.
+
+Results:
+- The two texts describing the topic in the wiki article are VERY close to each other (0.8398)
+  - This implies that the underlying topic, despite talking about different parts of it, is encoded in the vectors
+- Two first section seems to be closer to the other words
+  - This implies that choosing the right context to embed is of importance w.r.t. accuracy
+- The custom, supposedly very precise, description of a the corona face mask, is very close to all concepts
+  - This implies that embedding the text might be reasonable
+- The custom, precise description is very close to the wikipedia articles, not as close to general masks
+  - This implies that these sections to cover semantic knowledge about the circumstances without being trained on COVID
+- Facemask and Face mask alone have almost no similary to the wiki articles
+  - This implies that embedding the word alone may not be precise enough
+  
+Matrix:
+[1.0, 0.8398, 0.1179, 0.1427, 0.3443, 0.1397, 0.6104]
+[0.8398, 1.0, 0.0, 0.0514, 0.3142, 0.0476, 0.5565]
+[0.1179, 0.0, 1.0, 0.9056, 0.781, 0.8695, 0.4708]
+[0.1427, 0.0514, 0.9056, 1.0, 0.8334, 0.9466, 0.4629]
+[0.3443, 0.3142, 0.781, 0.8334, 1.0, 0.8444, 0.5279]
+[0.1397, 0.0476, 0.8695, 0.9466, 0.8444, 1.0, 0.4656]
+[0.6104, 0.5565, 0.4708, 0.4629, 0.5279, 0.4656, 1.0]
+"""
+fm1 = en(contexts["theme"]["face mask"]["text"][0])
+fm2 = en(contexts["theme"]["face mask"]["text"][1])
+fm3 = en("facemask")
+fm4 = en("face mask")
+fm5 = en("surgical mask")
+fm6 = en("mask")
+fm7 = en("mouth-nose-covering used to prevent viral spread")
+all_words1 = [fm1, fm2, fm3, fm4, fm5, fm6, fm7]
+sim_matrix1 = similarity_matrix(all_words1)
+print(*sim_matrix1, sep="\n")
+
+"""
+Experiment 003
+
+Setting:
+We now look at the context "theme" and one of its topics "social distancing".
+
+Hypothesis:
+Same as before.
+
+Results:
+We can see similar results as above.
+The encoded topics are all quite similar to each other.
+The first encoded topic is the best performing w.r.t. the other topics.
+Encoded Topic 2 and 3 have almost 0 similarity with the hardcoded encoded words.
+  - This implies that there are definitely bad candidate sentences.
+  - This implies that there is a need for evaluating the best performing textual representation
+If we only look at the synonyms we see that all encodings are very close to each other
+with a score of 0.7 - 0.9.
+  - This implies that encoding a very particular word may not be that important.
+Matrix:
+[1.0000, 0.6898, 0.6943, 0.7919, 0.3188, 0.2143, 0.1944, 0.2310, 0.2633, 0.3341, 0.3174, 0.3163]
+[0.6898, 1.0000, 0.5752, 0.7837, 0.0723, 0.0529, 0.0004, 0.0436, 0.0559, 0.0942, 0.0621, 0.0201]
+[0.6943, 0.5752, 1.0000, 0.5847, 0.0905, 0.0552, 0.0821, 0.0431, 0.0429, 0.1190, 0.1124, 0.1262]
+[0.7919, 0.7837, 0.5847, 1.0000, 0.3147, 0.1441, 0.2077, 0.2037, 0.2474, 0.3431, 0.3187, 0.1973]
+[0.3188, 0.0723, 0.0905, 0.3147, 1.0000, 0.8677, 0.7550, 0.7949, 0.7975, 0.8075, 0.8187, 0.7211]
+[0.2143, 0.0529, 0.0552, 0.1441, 0.8677, 1.0000, 0.7859, 0.8514, 0.7194, 0.8115, 0.8280, 0.7862]
+[0.1944, 0.0004, 0.0821, 0.2077, 0.7550, 0.7859, 1.0000, 0.8705, 0.6632, 0.8490, 0.8751, 0.7405]
+[0.2310, 0.0436, 0.0431, 0.2037, 0.7949, 0.8514, 0.8705, 1.0000, 0.7904, 0.9105, 0.9346, 0.8691]
+[0.2633, 0.0559, 0.0429, 0.2474, 0.7975, 0.7194, 0.6632, 0.7904, 1.0000, 0.7319, 0.7447, 0.6668]
+[0.3341, 0.0942, 0.1190, 0.3431, 0.8075, 0.8115, 0.8490, 0.9105, 0.7319, 1.0000, 0.9917, 0.8196]
+[0.3174, 0.0621, 0.1124, 0.3187, 0.8187, 0.8280, 0.8751, 0.9346, 0.7447, 0.9917, 1.0000, 0.8333]
+[0.3163, 0.0201, 0.1262, 0.1973, 0.7211, 0.7862, 0.7405, 0.8691, 0.6668, 0.8196, 0.8333, 1.0000]
+"""
+sd1 = en(contexts["theme"]["social distancing"]["text"][0])
+sd2 = en(contexts["theme"]["social distancing"]["text"][1])
+sd3 = en(contexts["theme"]["social distancing"]["text"][2])
+sd4 = en(contexts["theme"]["social distancing"]["text"][3])
+sd5 = en("social distancing")
+sd6 = en("social distance")
+sd7 = en("not going to public areas")
+sd8 = en("avoiding physical contact")
+sd9 = en("cutting oneself off")
+sd10 = en("avoiding contacts with people")
+sd11 = en("avoiding contacts with individuals")
+sd12 = en("avoiding crowded places")
+sd13 = en("A drastic measure of social distancing avoiding physical contact with other people and crowded places.")
+all_words2 = [eval(f"sd{i}") for i in range(1,13)]
+sim_matrix2 = similarity_matrix(all_words2)
+print(*sim_matrix2, sep="\n")
+
+
+"""
+General Notes:
+- it is of very importance to find out how to embed the topics the best way
+	- embedding wikipedia articles yields different results. some are really good (w.r.t the solely embedded terms) and some are really bad. Wikipedia articles might be too general 
+	for the model to capture the notion / sense behind it
+	- embedding described topics by hand with precise and many synomys might be good
+	- embedding the most general concept that describes the topic might be the best
+- different models yield different vector spaces
+- Pre-trained models only know general concepts. Corona specific words are unknown and have no sense in the vector space 
+	- Possibly map specific terms to general terms
+"""
--- context_concepts.py
+++ context_concepts.py
@@ -1,32 +1,38 @@
-contexts = [
-  { "theme" : [ { "topic_name" : "face mask",
-                  "text": "wiki text here",
-                  "emb" : [1.0, 1.0, 1.0]
-                },
-                { "topic_name" : "virus transmission",
-                  "text": "wiki text here",
-                  "emb" : [1.0, 1.0, 1.0]
-                }
-              ]
-  },
-  { "location" : [ { "topic_name" : "face mask",
-                  "text": "wiki text here",
-                  "emb" : [1.0, 1.0, 1.0]
-                },
-                { "topic_name" : "virus transmission",
-                  "text": "wiki text here",
-                  "emb" : [1.0, 1.0, 1.0]
-                }
-              ]
-  },
-  { "role" : [ { "topic_name" : "face mask",
-                  "text": "wiki text here",
-                  "emb" : [1.0, 1.0, 1.0]
-                },
-                { "topic_name" : "virus transmission",
-                  "text": "wiki text here",
-                  "emb" : [1.0, 1.0, 1.0]
-                }
-              ]
-  }
-]
+contexts = {
+    "theme": {
+        "face mask": {
+            "text": [
+                "The CDC and WHO recommend individuals wear non-medical face coverings in public settings where there is an increased risk of transmission and where social distancing measures are difficult to maintain. This recommendation is meant to reduce the spread of the disease by asymptomatic and pre-symtomatic individuals and is complementary to established preventive measures such as social distancing. Face coverings limit the volume and travel distance of expiratory droplets dispersed when talking, breathing, and coughing. Many countries and local jurisdictions encourage or mandate the use of face masks or cloth face coverings by members of the public to limit the spread of the virus.",
+                "Masks are also strongly recommended for those who may have been infected and those taking care of someone who may have the disease. When not wearing a mask, the CDC recommends covering the mouth and nose with a tissue when coughing or sneezing and recommends using the inside of the elbow if no tissue is available. Proper hand hygiene after any cough or sneeze is encouraged. Healthcare professionals interacting directly with COVID-19 patients are advised to use respirators at least as protective as NIOSH-certified N95 or equivalent, in addition to other personal protective equipment.",
+                ],
+            "emb": []
+        },
+        "hand washing": {
+            "text": [
+                "Hand washing is recommended to prevent the spread of the disease. The CDC recommends that people wash hands often with soap and water for at least twenty seconds, especially after going to the toilet or when hands are visibly dirty; before eating; and after blowing one's nose, coughing, or sneezing. This is because outside the human body, the virus is killed by household soap, which bursts its protective bubble. In addition, soap and water disrupts the sticky bond between pathogens and human skin which causes the coronavirus pathogen to slide off the hands/body. CDC has recommended using an alcohol-based hand sanitiser with at least 60 percent alcohol by volume when soap and water are not readily available.[136] The WHO advises people to avoid touching the eyes, nose, or mouth with unwashed hands. It is not clear whether washing hands with ash, if soap is not available, is effective at reducing the spread of viral infections.",
+                ],
+            "emb": []
+        },
+        "social distancing": {
+            "text": [
+                "Social distancing (also known as physical distancing) includes infection control actions intended to slow the spread of disease by minimising close contact between individuals. Methods include quarantines; travel restrictions; and the closing of schools, workplaces, stadiums, theatres, or shopping centres. Individuals may apply social distancing methods by staying at home, limiting travel, avoiding crowded areas, using no-contact greetings, and physically distancing themselves from others. Many governments are now mandating or recommending social distancing in regions affected by the outbreak. Non-cooperation with distancing measures in some areas has contributed to the further spread of the pandemic.",
+                "The maximum gathering size recommended by U.S. government bodies and health organisations was swiftly reduced from 250 people (if there were no known COVID-19 spread in a region) to 50 people, and later to 10. On 22 March 2020, Germany banned public gatherings of more than two people.A Cochrane review found that early quarantine with other public health measures are effective in limiting the pandemic, but the best manner of adopting and relaxing policies are uncertain, as local conditions vary.",
+                "Older adult and those with underlying medical conditions such as diabetes, heart disease, respiratory disease, hypertension, and compromised immune systems face increased risk of serious illness and complications and have been advised by the CDC to stay home as much as possible in areas of community outbreak.",
+                "In late March 2020, the WHO and other health bodies began to replace the use of the term 'social distancing' with 'physical distancing', to clarify that the aim is to reduce physical contact while maintaining social connections, either virtually or at a distance. The use of the term 'social distancing' had led to implications that people should engage in complete social isolation, rather than encouraging them to stay in contact through alternative means. Some authorities have issued sexual health guidelines for the pandemic, which include recommendations to have sex only with someone you live with, and who does not have the virus or symptoms of the virus.",
+                ],
+            "emb": []
+        },
+        "surface cleaning": {
+            "text": [
+                "Surfaces may be decontaminated with a number of solutions (within one minute of exposure to the disinfectant for a stainless steel surface), including 62–71 percent ethanol, 50–100 percent isopropanol, 0.1 percent sodium hypochlorite, 0.5 percent hydrogen peroxide, and 0.2–7.5 percent povidone-iodine. Other solutions, such as benzalkonium chloride and chlorhexidine gluconate, are less effective. Ultraviolet germicidal irradiation may also be used. The CDC recommends that if a COVID-19 case is suspected or confirmed at a facility such as an office or day care, all areas such as offices, bathrooms, common areas, shared electronic equipment like tablets, touch screens, keyboards, remote controls, and ATM machines used by the ill persons should be disinfected.",
+            ],
+            "emb": []
+        },
+        "quarantine": {
+            "text": [
+                "Self-isolation at home has been recommended for those diagnosed with COVID-19 and those who suspect they have been infected. Health agencies have issued detailed instructions for proper self-isolation. Many governments have mandated or recommended self-quarantine for entire populations. The strongest self-quarantine instructions have been issued to those in high risk groups. Those who may have been exposed to someone with COVID-19 and those who have recently travelled to a country or region with the widespread transmission have been advised to self-quarantine for 14 days from the time of last possible exposure.",
+            ],
+            "emb": []
+        },
+    }
+}
